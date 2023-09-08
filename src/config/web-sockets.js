01775cae78af8a57450502b6b8eb48434a8ebc44
LTorrealbaUpstart13 <ltorrealba@upstart13.com>
2023-09-08 12:51:45 -0300

    changes to make it work

--- src/config/web-sockets.js
+++ src/config/web-sockets.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
 
-const STRAPI_ENDPOINT = 'http://localhost:1337';
+const STRAPI_ENDPOINT = 'https://api.vecimarket.com';
 export const socket = io(STRAPI_ENDPOINT);
--- src/screens/ChatRoom/index.js
+++ src/screens/ChatRoom/index.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Input } from 'antd';
+import styled from 'styled-components';
+import { socket } from '../../config/web-sockets';
 import Header from '../../components/Header';
 import Messages from '../../components/Messages';
-import List from '../../components/List';
-import { socket } from '../../config/web-sockets';
 import { history } from '../../config/network';
 import {
     ChatContainer,
@@ -12,14 +11,66 @@ import {
     StyledButton,
     SendIcon
 } from './styles';
+import { Button, Input } from 'antd';
 
 function ChatRoom(props) {
-   
-    return (
-        <>
-        </>
-        
-    );
-};
+    const { username, room, joinData } = props;
+    const [messages, setMessages] = useState([]);
+    const [message, setMessage] = useState([]);
+    const [users, setUsers] = useState([]);
 
+    useEffect(() => {
+        if (Object.keys(joinData).length > 0) {
+            setMessages([joinData])
+            socket.on('message', (message, error) => {
+                setMessages(msgs => [...msgs, message]);
+            });
+        }
+        else {
+            history.push('/join')
+        }
+    }, [joinData]);
+
+    const handleChange = (e) => {
+        setMessage(e.target.value);
+    }
+
+    const handleClick = (e) => {
+        sendMessage(message);
+    }
+
+    const sendMessage = (message) => {
+        
+        if (message) {
+            socket.emit('sendMessage', { user: username, message, room }, (error) => {
+                if (error) {
+                    alert(error)
+                    history.push('/join');
+                }
+            });
+            setMessage('')
+        } else {
+            alert("Message can't be empty")
+        }
+    }
+
+    return (
+        <ChatContainer>
+            <Header room={room} />
+            <StyledContainer>
+                <ChatBox>
+                    <Messages
+                        messages={messages}
+                        username={username}
+                    />
+                    <Input type='text' placeholder='type your message' value={message} onChange={handleChange} />
+                    
+                    <SendIcon onClick={handleClick}>
+                        <i className='fa fa-paper-plane' />
+                    </SendIcon>
+                </ChatBox>
+            </StyledContainer>
+        </ChatContainer>
+    )
+};
 export default ChatRoom;
